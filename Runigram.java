d5ac291fdfa1eae7d57ce38e9feb44a33df8fd9c
RonIsakov <ron.isakov@post.runi.ac.il>
2024-01-30 19:17:13 +0200

    done????

--- Runigram.java
+++ Runigram.java
@@ -35,12 +35,12 @@ public class Runigram {
 		int numCols = in.readInt();
 		int numRows = in.readInt();
 		in.readInt();
+		// Creates the image array
 		Color[][] image = new Color[numRows][numCols];
 		// Reads the RGB values from the file, into the image array. 
 		// For each pixel (i,j), reads 3 values from the file,
 		// creates from the 3 colors a new Color object, and 
 		// makes pixel (i,j) refer to that object.
-		//// Replace the following statement with your code.
 		for(int i=0 ; i<numRows ; i++)
 			for(int j=0 ; j<numCols ; j++)
 				image[i][j] = (new Color(in.readInt(), in.readInt(), in.readInt()));
