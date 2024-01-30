5f0d28e2e0c81c10a5fe573a5e3b7b8b096977a1
RonIsakov <ron.isakov@post.runi.ac.il>
2024-01-30 19:09:17 +0200

    done?

---
+++ Editor4.java
@@ -0,0 +1,15 @@
+public class Editor4 {
+    public static void main (String[] args) {
+        //taking the file name and n from the user
+		String fileName = args[0];
+		int n = Integer.parseInt(args[1]);
+        //reading the image
+		Color[][] image = Runigram.read(fileName);
+        //creating an array witch is the image  grey
+		Color[][] grey = Runigram.grayScaled(image);
+        //morphing the images
+		Runigram.morph(image, grey, n);
+	}
+}
+
+
--- Runigram.java
+++ Runigram.java
@@ -157,7 +157,7 @@ public class Runigram {
 	 * The two images must have the same dimensions.
 	 */
 	public static Color[][] blend(Color[][] image1, Color[][] image2, double alpha) {
-		Color [][] newImage = new Color [image.length][image[0].length];
+		Color [][] newImage = new Color [image1.length][image2[0].length];
 		for(int i=0; i<image1.length;i++){
 			for(int j=0; j<image1[0].length; j++){
 				newImage[i][j] = blend(image1[i][j],image2[i][j],alpha);
