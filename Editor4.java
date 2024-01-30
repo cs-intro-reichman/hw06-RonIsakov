2456f01f0400205295e2ab0dfe6e03afefaffcce
RonIsakov <ron.isakov@post.runi.ac.il>
2024-01-30 19:29:20 +0200

    done?????

--- Editor4.java
+++ Editor4.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 public class Editor4 {
     public static void main (String[] args) {
         //taking the file name and n from the user
@@ -7,6 +8,8 @@ public class Editor4 {
 		Color[][] image = Runigram.read(fileName);
         //creating an array witch is the image  grey
 		Color[][] grey = Runigram.grayScaled(image);
+        //setting canvas
+        Runigram.setCanvas(image);
         //morphing the images
 		Runigram.morph(image, grey, n);
 	}
