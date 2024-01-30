bc60409bfc0e575d28f6702f15f78df4ad8b0a22
RonIsakov <ron.isakov@post.runi.ac.il>
2024-01-30 19:39:25 +0200

    ok?

--- Runigram.java
+++ Runigram.java
@@ -10,22 +10,19 @@ public class Runigram {
 		//// Hide / change / add to the testing code below, as needed.
 		
 		// Tests the reading and printing of an image:	
-		Color[][] image = read("ironman.ppm");
+		Color[][] image1 = read("ironman.ppm");
 		//print(tinypic);
-
+		Color[][] image2 = read("thor.ppm");
 		// Creates an image which will be the result of various 
 		// image processing operations:
-		Color[][] imageOut;
+		//Color[][] imageOut;
 
 		// Tests the horizontal flipping of an image:
-		imageOut = flippedHorizontally(image);
+		//imageOut = flippedHorizontally(image);
 		//System.out.println();
 		//print(imageOut);
-		setCanvas(image);
-		display(image);
-		StdDraw.pause(3000);
-		display(imageOut);
-
+		setCanvas(image1);
+		morph(image1,image2,5);
 		
 		//// Write here whatever code you need in order to test your work.
 		//// You can reuse / overide the contents of the imageOut array.
